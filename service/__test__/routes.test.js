db376b592ba85befa1eb774d1cc02dbc28af703c
theurbanFork <theurbanfork01@gmail.com>
2025-01-13 13:47:15 +0530

    Update routes.test.js

--- service/__test__/routes.test.js
+++ service/__test__/routes.test.js
@@ -1,30 +1,12 @@
 const request = require("supertest");
-const app = require("../../server"); // Import your app
-let server;
-
-beforeAll(() => {
-  // Start the server before tests
-  server = app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
-  });
-});
-
-afterAll(async () => {
-  // Ensure the server is closed after all tests are done
-  await server.close();
-});
+const app = require("../../server"); 
 
 afterEach(() => {
-  // Reset mocks after each test (if using any)
+  
   jest.restoreAllMocks();
 });
 
 describe("Testing routes", () => {
-  test("GET / - should return 200 if user accesses the home page", async () => {
-    jest.setTimeout(10000); // Increase timeout if necessary
-    const res = await request(app).get("/"); // Make the request
-    expect(res.statusCode).toBe(200); // Check if status is 200
-  });
 
   test("GET /orderpool - should return 401 if user is not logged in", async () => {
     jest.setTimeout(10000);
